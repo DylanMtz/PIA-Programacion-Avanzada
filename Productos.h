3696fe286f560030067f76c70343bc5454e95a57
ray.esro <ray.esro@gmail.com>
2021-10-19 20:43:01 -0500

    Se crea una funcion para crear, inicializar y devoler ese producto

--- Productos.h
+++ Productos.h
@@ -17,10 +17,12 @@ struct productos {
 	productos* prevProducto;
 }*oProducto, * aProducto;
 
+char fotoProducto1[MAX_PATH] = "";
+char fotoProducto2[MAX_PATH] = "";
 
 /// <summary>
 /// Si el nombre coincide con algun producto, entonces aProducto sera diferente de NULL,
-/// pero si no existe ese nombre entonces aProducto contendra NULL
+/// pero si no existe ese nombre ent
 /// </summary>
 /// <param name="nombreDelProducto"></param>
 void buscarProductoPorNombre(string nombreDelProducto) {
@@ -31,6 +33,42 @@ void buscarProductoPorNombre(string nombreDelProducto) {
 		}
 		aProducto = aProducto->nextProducto;
 	}
+}
+
+/// <summary>
+/// funcion que crea un nuevo producto, le asigna sus valores, y le asigna NULL a los enlaces
+/// </summary>
+/// <param name="nombreDelProducto"></param>
+/// <param name="cantidadEnInventario"></param>
+/// <param name="codigoDelProducto"></param>
+/// <param name="marcaDelProducto"></param>
+/// <param name="descripcionDelProducto"></param>
+/// <param name="precio"></param>
+/// <returns>Retorna un nuevo producto</returns>
+productos* crearProducto(
+	string nombreDelProducto,
+	string cantidadEnInventario,
+	string codigoDelProducto,
+	string marcaDelProducto,
+	string descripcionDelProducto,
+	string precio
+) {
+
+	productos* nuevoProducto = new productos;
+	nuevoProducto->nombreProducto = nombreDelProducto;
+	nuevoProducto->cantidadProducto = cantidadEnInventario;
+	nuevoProducto->codigoProducto = codigoDelProducto;
+	nuevoProducto->marcaProducto = marcaDelProducto;
+	nuevoProducto->precioProducto = precio;
+	nuevoProducto->IDProducto = 0;
+	for (int i = 0; i < MAX_PATH; i++) {
+		oProducto->descripcionProducto[i] = desc[i];
+		oProducto->fotoP1[i] = fotoProducto1[i];
+		oProducto->fotoP2[i] = fotoProducto2[i];
+	}
+	nuevoProducto->nextProducto = NULL;
+	nuevoProducto->prevProducto = NULL;
+	return nuevoProducto;
 }
 
 #endif // !_PRODUCTOS_H_
